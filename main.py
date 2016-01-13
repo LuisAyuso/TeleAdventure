086350b4666caee016e43c2836b894be82122cc3
luis <luis@dps.uibk.ac.at>
2016-01-13 15:25:17 +0100

    first commands, play with ipc now

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 test test test
 """
 
-from telegram import (Updater, ChatAction, Document)
+from telegram import (Updater, ChatAction)
 import ConfigParser
 import os
 import logging
+
+from subprocess import Popen, PIPE
 
 
 # Enable logging
@@ -15,15 +17,29 @@ logging.basicConfig(format=formatstr, level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
+active_games = dict()
+
+
+class Game:
+
+    game_stream = None
+
+    def __init__(self):
+        print "new game"
+
+        process = Popen(['frotz', '/home/luis/code/tele-adventure/games/zork_1.z5', '-d'], stdout=PIPE, stderr=PIPE)
+
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Hola hola!')
+    bot.sendMessage(update.message.chat_id, text='Hi, lets play something!')
 
 
 def help(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Help! i need somebody')
+    bot.sendMessage(update.message.chat_id, text='Help! i need somebody:')
+    bot.sendMessage(update.message.chat_id, text='/list_games: what games are installed')
+    bot.sendMessage(update.message.chat_id, text='/start_game: start a new game')
 
 
 def echo(bot, update):
@@ -55,20 +71,6 @@ def process_message_closure(config):
     def process_message(bot, update):
 
         bot.sendChatAction(update.message.chat_id, ChatAction.TYPING)
-
-        downloads = config["downloads"]
-
-        if len(update.message.text) != 0:
-            print "message with text", update.message.text
-        if update.message.photo is not None:
-            print "message with photo"
-        if update.message.video is not None:
-            print "message with video"
-        if update.message.document is not None:
-            print "message with docu", update.message.document
-            f = bot.getFile(update.message.document.file_id)
-            print "file: ", f
-            f.download(downloads + "/" + update.message.document.file_name)
         echo(bot, update)
 
     return process_message
@@ -78,43 +80,33 @@ def ls_closure(config):
 
     def ls_command_handle(bot, update):
 
-        downloads = config["downloads"]
-        for f in os.listdir(downloads):
+        games = config["games"]
+        for f in os.listdir(games):
             bot.sendMessage(update.message.chat_id, f)
 
     return ls_command_handle
 
 
-def get_closure(config):
+def start_closure(config):
 
-    def get_command_handle(bot, update):
+    def start_command_handle(bot, update):
 
-        downloads = config["downloads"]
-
-        filename = update.message.text[4:]
-        filename = downloads + "/" + filename.lstrip()
-
-        if os.path.exists(filename):
-            bot.sendMessage(update.message.chat_id, "file exists! " + filename)
-         "   f = open(filename, 'r')
-         "   dump = f.read()
-
-         "   Document.de_json(dump)
-
-         "   bot.sendDocument
-
+        # make sure user is not playing
+        user = update.message.from_user
+        if user.id not in active_games:
+            active_games[user.id] = Game()
         else:
-            bot.sendMessage(update.message.chat_id, "file not found " + filename)
+            bot.sendMessage(update.message.chat_id, "already in game")
 
-    return get_command_handle
+    return start_command_handle
 
 
 def main():
 
     cnf = loadConfig()
     TOKEN = cnf["token"]
-    downloads = cnf["downloads"]
-    print "donwloads in: ", downloads
+    games = cnf["games"]
+    print "donwloads in: ", games
 
     # Create the EventHandler and pass it your bot's token.
     updater = Updater(TOKEN)
@@ -125,8 +117,8 @@ def main():
     # on different commands - answer in Telegram
     dp.addTelegramCommandHandler("start", start)
     dp.addTelegramCommandHandler("help", help)
-    dp.addTelegramCommandHandler("ls", ls_closure(cnf))
-    dp.addTelegramCommandHandler("get", get_closure(cnf))
+    dp.addTelegramCommandHandler("list_games", ls_closure(cnf))
+    dp.addTelegramCommandHandler("start_game", start_closure(cnf))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.addTelegramMessageHandler(process_message_closure(cnf))
